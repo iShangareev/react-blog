1df50644023b8f8a7bff6c5b2e754b9e17fbff97
Ильнар Шангареев <i.shangareyev@modulbank.ru>
2022-05-23 16:10:00 +0400

    декомпозиция

--- src/App.js
+++ src/App.js
@@ -7,23 +7,34 @@ import MyModal from './components/UI/MyModal/MyModal';
 import MyButton from './components/UI/button/MyButton';
 import PostsService from './API/PostService';
 import Loader from './components/UI/Loader/Loader';
+import Pagination from './components/pagination/Pagination';
 import { useFetching } from './hooks/useFetching';
 import {usePosts} from './hooks/usePosts'
+import { getPageCount, getPagesArray } from './utils/pages';
 
 function App() {
   const [posts, setPosts] = useState([])
 
   const [filter, setFilter] = useState({sort: '', query: ''})
   const [modal, setModal] = useState(false)
+  const [totalPages, setTotalPages] = useState(0)
+  const [limit, setLimit] = useState(10)
+  const [page, setPage] = useState(1)
+
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+  let pagesArray = getPagesArray(totalPages)
+
+
   const [fetchPosts, isPostLoading, postError] = useFetching( async () => {
-    const posts = await PostsService.getAll()
-    setPosts(posts)
+    const response = await PostsService.getAll(limit, page)
+    setPosts(response.data)
+    const totalCount = response.headers['x-total-count']
+    setTotalPages(getPageCount(totalCount, limit))
   })
 
   useEffect(() => {
     fetchPosts()
-  },[])
+  },[ page ])
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -32,6 +43,10 @@ function App() {
 
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id))
+  }
+
+  const changePage = (page) => {
+    setPage(page)
   }
 
   return (
@@ -56,6 +71,11 @@ function App() {
            </div>
         : <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'Список постов'}/>
       }
+      <Pagination
+        page={page}
+        changePage={changePage}
+        totalPages={totalPages}
+      />
     </div>
   );
 }
